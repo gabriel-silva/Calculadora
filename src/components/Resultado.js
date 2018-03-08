b7af9951105a2ea32b91a87fbbb91f6406cd30da
Gabriel Silva <gabriel-silva_2011@hotmail.com>
2018-03-08 09:52:02 -0300

    adicionando o component visor

--- src/components/Resultado.js
+++ src/components/Resultado.js
@@ -3,11 +3,13 @@ import {
     View,
     Text
 } from 'react-native';
+import Visor from './Visor';
 
 const Resultado = props => (
-    <View>
-        <Text>Resultado</Text>
-    </View>
+    // <View>
+    //     <Text>Resultado</Text>
+    // </View>
+    <Visor />
 );
 
 //parecido como Resultado: Resultado
---
+++ src/components/Visor.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+    View,
+    Text,
+    TextInput,
+    StyleSheet
+} from 'react-native';
+
+export default props => (
+    <View>
+        <TextInput style={styles.visor}
+            placeholder='Resultado'
+        //editable={false}
+        />
+    </View>
+);
+
+const styles = StyleSheet.create({
+    visor: {
+        height: 100,
+        fontSize: 30
+    }
+});
