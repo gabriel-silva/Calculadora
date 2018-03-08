97b3b2a26f81d582b9459573928f9e596a121b0a
Gabriel Silva <gabriel-silva_2011@hotmail.com>
2018-03-08 11:44:52 -0300

    Função Bind

--- src/components/Painel.js
+++ src/components/Painel.js
@@ -11,11 +11,15 @@ class Painel extends Component {
 
     constructor(props) {
         super(props);
-        this.state = { num1: '', num2: '' };
+        this.state = { num1: '10', num2: '25' };
+
+        //preservando o contexto da função calcular
+        this.calcular = this.calcular.bind(this);
     }
 
     calcular() {
-        console.log('Sim, vamos calcular');
+        const resultado = parseFloat(this.state.num1) + parseFloat(this.state.num2);
+        console.log(resultado);
     }
 
     render() {
