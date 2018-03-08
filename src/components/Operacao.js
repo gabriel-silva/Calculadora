1249b5b4c81c07481308d98acef0149782491bbb
Gabriel Silva <gabriel-silva_2011@hotmail.com>
2018-03-08 10:30:46 -0300

    correções no component operacao

--- src/components/Operacao.js
+++ src/components/Operacao.js
@@ -1,8 +1,19 @@
 import React from 'react';
-import { 
-    Text
+import {
+    Picker,
+    StyleSheet
 } from 'react-native';
 
 export default props => (
-    <Text> Operacao </Text>
+    <Picker style={styles.operacao}>
+        <Picker.Item label='Soma' value='soma' />
+        <Picker.Item label='Subtração' value='subtracao' />
+    </Picker>
 );
+
+const styles = StyleSheet.create({
+    operacao: {
+        marginTop: 15,
+        marginBottom: 15
+    }
+});
